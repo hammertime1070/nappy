5525a672982366bfa1a44246a37dfe3d50358c25
MKeith <mbkeith614@gmail.com>
2024-06-05 12:04:15 -0400

    First Blog Post

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, spawn_player))
         .add_systems(Update, player_movement)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands) {
     // Camera Setup
     commands.spawn((Camera2dBundle {
         transform: Transform::from_xyz(100.0, 200.0, 0.0),
@@ -16,41 +16,25 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     },
     MyCameraMarker,
     ));
-    // Load sprites
-    commands.spawn(SpriteBundle {
-        texture:asset_server.load("player.png"),
-        ..default()
-    });
-
-    // Draw the Player
-    let player_sprite = asset_server.load("player.png");
-    commands.insert_resource(PlayerSprite(player_sprite.clone()));
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.add(player_sprite.into()),
-        transform: Transform: Vec3::new(0.0, 0.0, 0.0),
-        ..default()
-    },
-    ..default())
-    .insert(Player);
 }
 
 fn player_movement(
-    keyboard_input: Res<Input<KeyCode>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<&mut Transform, With<Player>>,
 ) {
     for mut transform in query.iter_mut() {
         let mut direction = Vec3::ZERO;
 
-        if keyboard_input.pressed(KeyCode::W) {
+        if keyboard_input.pressed(KeyCode::KeyW) {
             direction.y += 1.;
         }
-        if keyboard_input.pressed(KeyCode::S) {
+        if keyboard_input.pressed(KeyCode::KeyS) {
             direction.y -= 1.;
         }
-        if keyboard_input.pressed(KeyCode::A) {
+        if keyboard_input.pressed(KeyCode::KeyA) {
             direction.x -= 1.;
         }
-        if keyboard_input.pressed(KeyCode::D) {
+        if keyboard_input.pressed(KeyCode::KeyD) {
             direction.x += 1.;
         }
 
@@ -61,9 +45,29 @@ fn player_movement(
 #[derive(Component)]
 struct MyCameraMarker;
 
+// Player
 #[derive(Component)]
 struct Player;
 
-struct PlayerSprite(Handle<Texture>);
+#[derive(Bundle)]
+struct PlayerBundle {
+    // Component used to identify an entity part of bevy core
+    name: Name,
+    // A way to tag the player
+    player: Player,
+    // Eventually we will convert this to a spritesheet
+    sprite: SpriteBundle,
+}
 
-//TODO: Accidentally broke everything
+fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let player_sprite = asset_server.load("player.png");
+    commands.spawn(PlayerBundle {
+        player: Player,
+        name: Name::new("Player"),
+        sprite: SpriteBundle {
+            texture: player_sprite,
+            transform: Transform::from_xyz(0.0, 0.0, 1.0),
+            ..default()
+        },
+    });
+}
